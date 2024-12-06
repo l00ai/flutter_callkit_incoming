ecbbb4fecd94b84ef785751145fb85dbb1cc96bf
l00ai <74601007+l00ai@users.noreply.github.com>
2024-12-06 22:28:55 +0200

    new

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
@@ -1,7 +1,6 @@
 package com.hiennv.flutter_callkit_incoming
 
 import android.app.Activity
-import android.app.ActivityManager
 import android.app.KeyguardManager
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -13,23 +12,20 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.PowerManager
+import android.text.TextUtils
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.view.animation.AnimationUtils
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import com.hiennv.flutter_callkit_incoming.widgets.RippleRelativeLayout
+import com.ncorti.slidetoact.SlideToActView
+import com.squareup.picasso.OkHttp3Downloader
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
-import kotlin.math.abs
 import okhttp3.OkHttpClient
-import com.squareup.picasso.OkHttp3Downloader
-import android.view.ViewGroup.MarginLayoutParams
-import android.os.PowerManager
-import android.text.TextUtils
-import android.util.Log
+import kotlin.math.abs
 
 
 class CallkitIncomingActivity : Activity() {
@@ -81,12 +77,7 @@ class CallkitIncomingActivity : Activity() {
 
     private lateinit var ivAvatar: CircleImageView
 
-    private lateinit var llAction: LinearLayout
-    private lateinit var ivAcceptCall: ImageView
-    private lateinit var tvAccept: TextView
-
-    private lateinit var ivDeclineCall: ImageView
-    private lateinit var tvDecline: TextView
+    private lateinit var swipe_btn: SlideToActView
 
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -213,43 +204,40 @@ class CallkitIncomingActivity : Activity() {
                     .into(ivAvatar)
         }
 
-        val callType = data?.getInt(CallkitConstants.EXTRA_CALLKIT_TYPE, 0) ?: 0
-        if (callType > 0) {
-            ivAcceptCall.setImageResource(R.drawable.ic_video)
-        }
         val duration = data?.getLong(CallkitConstants.EXTRA_CALLKIT_DURATION, 0L) ?: 0L
         wakeLockRequest(duration)
 
         finishTimeout(data, duration)
 
         val textAccept = data?.getString(CallkitConstants.EXTRA_CALLKIT_TEXT_ACCEPT, "")
-        tvAccept.text = if (TextUtils.isEmpty(textAccept)) getString(R.string.text_accept) else textAccept
-        val textDecline = data?.getString(CallkitConstants.EXTRA_CALLKIT_TEXT_DECLINE, "")
-        tvDecline.text = if (TextUtils.isEmpty(textDecline)) getString(R.string.text_decline) else textDecline
+        swipe_btn.text = if (TextUtils.isEmpty(textAccept)) getString(R.string.text_accept) else textAccept.toString()
 
-		try {
-			tvAccept.setTextColor(Color.parseColor(textColor))
-			tvDecline.setTextColor(Color.parseColor(textColor))
-		} catch (error: Exception) {
-		}
+//        tvAccept.text = if (TextUtils.isEmpty(textAccept)) getString(R.string.text_accept) else textAccept
+//
+//		try {
+//			tvAccept.setTextColor(Color.parseColor(textColor))
+//		} catch (error: Exception) {
+//		}
+
+        //swipe_btn?.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary))
 
         val backgroundColor = data?.getString(CallkitConstants.EXTRA_CALLKIT_BACKGROUND_COLOR, "#0955fa")
         try {
             ivBackground.setBackgroundColor(Color.parseColor(backgroundColor))
         } catch (error: Exception) {
         }
-        var backgroundUrl = data?.getString(CallkitConstants.EXTRA_CALLKIT_BACKGROUND_URL, "")
-        if (backgroundUrl != null && backgroundUrl.isNotEmpty()) {
-            if (!backgroundUrl.startsWith("http://", true) && !backgroundUrl.startsWith("https://", true)){
-                backgroundUrl = String.format("file:///android_asset/flutter_assets/%s", backgroundUrl)
-            }
-            val headers = data?.getSerializable(CallkitConstants.EXTRA_CALLKIT_HEADERS) as HashMap<String, Any?>
-            getPicassoInstance(this@CallkitIncomingActivity, headers)
-                    .load(backgroundUrl)
-                    .placeholder(R.drawable.transparent)
-                    .error(R.drawable.transparent)
-                    .into(ivBackground)
-        }
+//        var backgroundUrl = data?.getString(CallkitConstants.EXTRA_CALLKIT_BACKGROUND_URL, "")
+//        if (backgroundUrl != null && backgroundUrl.isNotEmpty()) {
+//            if (!backgroundUrl.startsWith("http://", true) && !backgroundUrl.startsWith("https://", true)){
+//                backgroundUrl = String.format("file:///android_asset/flutter_assets/%s", backgroundUrl)
+//            }
+//            val headers = data?.getSerializable(CallkitConstants.EXTRA_CALLKIT_HEADERS) as HashMap<String, Any?>
+//            getPicassoInstance(this@CallkitIncomingActivity, headers)
+//                    .load(backgroundUrl)
+//                    .placeholder(R.drawable.transparent)
+//                    .error(R.drawable.transparent)
+//                    .into(ivBackground)
+//        }
     }
 
     private fun finishTimeout(data: Bundle?, duration: Long) {
@@ -284,33 +272,18 @@ class CallkitIncomingActivity : Activity() {
 //        tvNameCaller = findViewById(R.id.tvNameCaller)
 //        tvNumber = findViewById(R.id.tvNumber)
         ivAvatar = findViewById(R.id.ivAvatar)
+        swipe_btn = findViewById(R.id.swip_btn)
 
-        llAction = findViewById(R.id.llAction)
 
-        val params = llAction.layoutParams as MarginLayoutParams
-        params.setMargins(0, 0, 0, Utils.getNavigationBarHeight(this@CallkitIncomingActivity))
-        llAction.layoutParams = params
 
-        ivAcceptCall = findViewById(R.id.ivAcceptCall)
-        tvAccept = findViewById(R.id.tvAccept)
-        ivDeclineCall = findViewById(R.id.ivDeclineCall)
-        tvDecline = findViewById(R.id.tvDecline)
-        animateAcceptCall()
+        swipe_btn.onSlideCompleteListener = (object : SlideToActView.OnSlideCompleteListener {
+            override fun onSlideComplete(view: SlideToActView) {
+                swipe_btn.setCompleted(completed = true, withAnimation = true)
+                onAcceptClick()
+            }
+        })
 
-        ivAcceptCall.setOnClickListener {
-            onAcceptClick()
-        }
-        ivDeclineCall.setOnClickListener {
-            onDeclineClick()
-        }
     }
-
-    private fun animateAcceptCall() {
-        val shakeAnimation =
-                AnimationUtils.loadAnimation(this@CallkitIncomingActivity, R.anim.shake_anim)
-        ivAcceptCall.animation = shakeAnimation
-    }
-
 
     private fun onAcceptClick() {
         val data = intent.extras?.getBundle(CallkitConstants.EXTRA_CALLKIT_INCOMING_DATA)
